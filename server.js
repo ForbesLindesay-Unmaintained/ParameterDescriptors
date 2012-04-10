c0f7252f3028bb9471d6838abaf0adaf35e96b18
= <=>
2012-04-10 18:46:38 +0100

    Complete and test the test server

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ var server = require("express").createServer(),
     path   = require("path"),
     fs     = require("fs");
 
+server.set('views', __dirname);
+server.set('view options', {
+	layout:false
+});
 
+server.get("/style.css", function(req,res,next){
+	res.sendfile('./default-styles.css',function(err){
+		console.log(err);
+	});
+});
+server.use(server.router);
 /*
  * Load the file in as text and set it as the property
  * req.source
  */
 server.use(function(req,res,next){
-	if(req.method !== 'GET') next();
+	if(req.method !== 'GET') res.send(req.method + " is not supported", 500);
 	else{
 		var filePath = "./samples" + url.parse(req.url).pathname;
 		path.exists(filePath, function(exists){
@@ -23,7 +33,7 @@ server.use(function(req,res,next){
 					}
 				});
 			} else {
-				res.send("Could not find requested content", 404);
+				res.send("File path "+filePath+" does not exist", 404);
 			}
 		});
 	}
@@ -45,6 +55,9 @@ server.use(function(req, res, next){
  * Use one single method to render all outputs
  */
 server.use(function(req,res,next){
-	if(req.parsed) res.send(parser.render(req.parsed));
+	if(req.parsed) res.render("server.ejs", {output:parser.render(req.parsed)});
 	else res.send("Could not find requested content", 404);
 });
+
+server.listen(process.env.PORT || 8080);
+console.log("server listening on "+(process.env.PORT||8080));
